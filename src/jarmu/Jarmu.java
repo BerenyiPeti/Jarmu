1e16efa81e44ad3aa8127535ffafe120fc648d85
BerenyiPeti <berenyipeti01@gmail.com>
2023-02-13 09:49:19 +0100

    alap metódusok

---
+++ src/jarmu/Jarmu.java
@@ -0,0 +1,21 @@
+package jarmu;
+
+public abstract class Jarmu {
+    private boolean beinditva, uzemanyag, megerkezett;
+
+    public void beindit() {
+        
+    }
+   
+    public void leallit() {
+        
+    }
+    
+    public boolean tankol() {
+        return true;
+    }
+    
+    public boolean halad() {
+        return true;
+    }
+}
