0155c3d8174be599f0568c49706c9338f916af58
BerenyiPeti <berenyipeti01@gmail.com>
2023-02-13 10:05:28 +0100

    auto konstruktor

--- src/modell/Auto.java
+++ src/modell/Auto.java
@@ -3,6 +3,10 @@ package modell;
 public class Auto extends Jarmu{
 
     private boolean defekt;
+
+    public Auto() {
+        
+    }
     
     public void kereketCserel() {
         
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -3,16 +3,9 @@ package modell;
 public abstract class Jarmu {
     private boolean beinditva, uzemanyag, megerkezett;
 
-    public Jarmu(boolean beinditva, boolean uzemanyag, boolean megerkezett) {
-        this.beinditva = beinditva;
-        this.uzemanyag = uzemanyag;
-        this.megerkezett = megerkezett;
-    }
-
     public Jarmu() {
+        
     }
-    
-    
     
     public void beindit() {
         
