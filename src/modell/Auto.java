4c8b6b6a307cc7643b889899623a02107559afbc
BerenyiPeti <berenyipeti01@gmail.com>
2023-02-13 09:53:49 +0100

    package javítva

---
+++ src/modell/Auto.java
@@ -0,0 +1,19 @@
+package modell;
+
+public class Auto {
+
+    public Auto(boolean defekt) {
+        this.defekt = defekt;
+    }
+    
+    private boolean defekt; 
+    
+    public void kereketCserel() {
+        
+    }
+    
+    public boolean halad() {
+        return true;
+    }
+    
+}
--- src/modell/Jarmu.java
+++ src/modell/Jarmu.java
@@ -1,4 +1,4 @@
-package jarmu;
+package modell;
 
 public abstract class Jarmu {
     private boolean beinditva, uzemanyag, megerkezett;
